47922fc65cf5ec85dc5552f31dd974872a9017fc
苯苯 <854363956@qq.com>
2022-04-26 08:54:34 +0800

    fix: 解决调试中 Linux 环境无法正确加载的问题

--- native/app.ts
+++ native/app.ts
@@ -1,5 +1,29 @@
 import { app, BrowserWindow } from 'electron'
 import ipcMainWindowConfig from './controllers/window'
+import request from 'axios'
+const sleep = (time: number) => {
+    return new Promise<void>((resolve) => {
+        setTimeout(() => {
+            resolve()
+        }, time)
+    })
+}
+
+const loadDevSource = async (win: BrowserWindow) => {
+    for (;;) {
+        try {
+            const resp = await request.get('http://127.0.0.1:9080/')
+            console.log(resp.status)
+            if (resp.status === 200) {
+                win.loadURL('http://localhost:9080')
+                return
+            }
+
+        } catch (error) {
+            sleep(800)
+        }
+    }
+}
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -14,7 +38,7 @@ const createWindow = () => {
         backgroundColor: '#252526'
     })
 
-    win.loadURL('http://localhost:9080')
+    loadDevSource(win)
 
     win.on('ready-to-show', () => {
         win.show()
